3d09c811bc69f8affd35b05595d664c0a25bbae0
yjs <wlstn4966@gmail.com>
2020-06-28 01:49:19 +0900

    BaseTimeEntity

--- src/main/java/com/yubron/board/springboot/domain/posts/Posts.java
+++ src/main/java/com/yubron/board/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.yubron.board.springboot.domain.posts;
 
+import com.yubron.board.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
