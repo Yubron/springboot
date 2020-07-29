1264238c32f035996293128c128d12ff3029815b
yjs <wlstn4966@gmail.com>
2020-07-29 21:35:43 +0900

    NAVER OAUTH COMP

--- src/main/java/com/yubron/board/springboot/web/IndexController.java
+++ src/main/java/com/yubron/board/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.yubron.board.springboot.web;
 
+import com.yubron.board.springboot.config.auth.LoginUser;
 import com.yubron.board.springboot.config.auth.dto.SessionUser;
 import com.yubron.board.springboot.service.PostsService;
 import com.yubron.board.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.ILoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +19,11 @@ public class IndexController {
 
     private final PostsService postsService;
     private final HttpSession httpSession;
+
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName",user.getName());
