478d1f582e1a7404570a8281b88170397a1510aa
yjs <wlstn4966@gmail.com>
2020-08-27 22:18:10 +0900

    Modify Main Screen

--- src/test/java/com/yubron/board/springboot/web/IndexControllerTest.java
+++ src/test/java/com/yubron/board/springboot/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/",String.class);
 
         //then
-        assertThat(body).contains("WebService starting with SpringBoot");
+        assertThat(body).contains("자 급 자 족");
     }
 }
